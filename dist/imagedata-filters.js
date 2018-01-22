f111eefc3b8a7539cb504a4c81164c19109173bc
harry <harry_tse@163.com>
2018-01-22 22:48:56 +0800

    change contrast algorithm

--- dist/imagedata-filters.js
+++ dist/imagedata-filters.js
@@ -315,18 +315,19 @@ function contrast$1 (imagedata, opts) {
     var opts = opts || {amount: 0};
     var amount = +opts.amount || 0;
     var r,g,b;
-    var intercept = -(.5 * amount) + .5;
-
     for (; i < len; i += 4) {
-        r = data[i] * amount + intercept;
-        g = data[i + 1] * amount + intercept;
-        b = data[i + 2] * amount + intercept;
+        // r = data[i] * amount + intercept
+        // g = data[i + 1] * amount + intercept
+        // b = data[i + 2] * amount + intercept
+        r = 128 + (data[i] - 128) * amount;
+        g = 128 + (data[i + 1] - 128) * amount;
+        b = 128 + (data[i + 2] - 128) * amount;
 
         data[i] = r;
         data[i + 1] = g;
         data[i + 2] = b;
     }
-    return imagedata    
+    return imagedata
 }
 
 // according to https://drafts.fxtf.org/filter-effects/#opacityEquivalent
--- src/filters/contrast.js
+++ src/filters/contrast.js
@@ -12,13 +12,16 @@ function contrast (imagedata, opts) {
     var intercept = -(.5 * amount) + .5
 
     for (; i < len; i += 4) {
-        r = data[i] * amount + intercept
-        g = data[i + 1] * amount + intercept
-        b = data[i + 2] * amount + intercept
+        // r = data[i] * amount + intercept
+        // g = data[i + 1] * amount + intercept
+        // b = data[i + 2] * amount + intercept
+        r = 128 + (data[i] - 128) * amount
+        g = 128 + (data[i + 1] - 128) * amount
+        b = 128 + (data[i + 2] - 128) * amount
 
         data[i] = r
         data[i + 1] = g
         data[i + 2] = b
     }
-    return imagedata    
+    return imagedata
 }
